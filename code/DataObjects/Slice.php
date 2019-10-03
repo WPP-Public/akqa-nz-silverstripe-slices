987fdd394f0072ee50bdf46eee6a64be08d255a2
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-10-04 10:24:07 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/DataObjects/Slice.php
+++ code/DataObjects/Slice.php
@@ -6,6 +6,20 @@ class Slice extends DataObject
         'previewer' => '%$DataObjectPreviewer'
     );
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'Slice';
+
     private static $db = array(
         'Template' => 'Varchar(255)',
         'VisualOptions' => 'Varchar(255)',
